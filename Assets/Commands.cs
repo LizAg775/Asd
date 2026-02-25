a4cad03559c9e3a3e9e0b8fb9f8f1e564d02b697
Liz Agatha <samuelygorcc@gmail.com>
2026-02-25 11:39:36 -0300

    Primeiro Comando e Fase Terminada

---
+++ Assets/Commands.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Commands : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.Translate(1, 0, 0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
