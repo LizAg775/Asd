25d810ed8bc87b56853f1148ceface5423be7979
Liz Agatha <samuelygorcc@gmail.com>
2026-02-26 08:59:44 -0300

    First Code, Phase Complete

--- Assets/New Version/Commands.cs
+++ Assets/New Version/Commands.cs
@@ -5,7 +5,7 @@ public class Commands : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.Translate(1, 0, 0);
+        transform.Translate(-1, 0, 0);
         
     }
 
