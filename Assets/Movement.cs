34c4a06cd2743575dad4200c6fa4d1a0630a7b43
Liz Agatha <samuelygorcc@gmail.com>
2026-02-26 11:39:01 -0300

    More Movement Coding

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,17 +1,19 @@
 using UnityEngine;
 
-public class Commands : MonoBehaviour
+public class Movement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.Translate(-1, 0, 0);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(-0.0005f, 0, 0);
+        }
     }
 }
