bad68063358c89f9b517bd3c632c0fd75775d368
Liz Agatha <samuelygorcc@gmail.com>
2026-03-12 11:39:47 -0300

    Eu quero chorar muito

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -10,23 +10,25 @@ public class Movement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        public posicaoInicial = Vector3
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("d")){
+
+        if(Input.GetKey("r")){
+            this.transform.position = posicaoInicial
+        }
+        if (Input.GetKey("d") && Physics.CheckSphere(transform.position + Vector3.right * 0.2f, 0.85f)== false) {
             this.transform.Translate(-0.05f, 0, 0);
         }
 
-        if (Input.GetKey("a")){
+        if (Input.GetKey("a") && Physics.CheckSphere(transform.position + Vector3.left * 0.2f, 0.85f) == false){
             this.transform.Translate(0.05f, 0, 0);
         }
 
-        
-
-        if (Input.GetKey("w") && Physics.CheckSphere(transform.position, 0, 0, 0.5f) && pulando == false){
+        if (Input.GetKey("w") && Physics.CheckSphere(transform.position + Vector3.down * 0.2f, 0.87f) && pulando == false){
             pulando = true;
             inicioPulo = frameAtual;
         }
@@ -35,15 +37,15 @@ public class Movement : MonoBehaviour
 
         if(pulando == true){
             if(framesPassados < 100){
-                this.transform.Translate(0, 0, 0.05f);
+            this.transform.Translate(0, 0, 0.05f);
             }else{
-                pulando = false;
+            pulando = false;
             }
         }
 
         frameAtual = frameAtual + 1;
 
-        if(pulando == false &&Physics.CheckSphere(transform.position, 1)==false){
+        if(pulando == false && Physics.CheckSphere(transform.position + Vector3.down * 0.2f, 0.87f) == false){
             this.transform.Translate(0, 0, -0.05f);
         }
 
